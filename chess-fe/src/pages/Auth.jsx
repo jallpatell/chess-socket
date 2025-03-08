74e55f890905df5d203b30df8cfaa8b3a720ae2d
jallpatell <135193901+jallpatell@users.noreply.github.com>
2025-03-08 10:35:32 +0530

    bad commit

---
+++ chess-fe/src/pages/Auth.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div>this is a auth pagex </div>
+  )
+}
+
+export default Auth
---
+++ chess-fe/src/pages/Board.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const board = () => {
+  const boardSize = 8;
+
+  return (
+    <div className="chessboard-container">
+    <h2>Chessboard</h2>
+    <div> </div>
+  </div>
+  );
+}
+
+export default board
